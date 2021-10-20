fd9ca32dd5122f61bc171e97658e4fce70a02461
Ekramul Islam Sumon <mmsumon799@gmail.com>
2021-10-20 23:53:46 +0600

    max value from array and strign

---
+++ js/math.js
@@ -0,0 +1,7 @@
+// Javascript max() function for 3 numbers
+
+console.log(Math.max(1, 2, 3, 4, 5, 6)); //  6
+
+let array = [1, 2, 3, 4, 5, 6, 10, 7];
+let max   = Math.max(...array);
+console.log(max); // 6
