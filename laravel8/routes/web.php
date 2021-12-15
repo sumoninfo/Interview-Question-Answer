0fa0d49cd59b67455b44acb9f7f06416f2af3fcc
Ekramul Sumon <38817030+sumoninfo@users.noreply.github.com>
2021-12-15 11:30:32 +0600

    laravel: collect => chunk

--- laravel8/routes/web.php
+++ laravel8/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $collection = collect([1, 2, 3, 4, 5, 6, 7]);
+
+    $chunks = $collection->chunk(4);
+
+   return $chunks->toArray();
     return view('welcome');
 });
